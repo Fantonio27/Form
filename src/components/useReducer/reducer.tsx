249818f7dc4cfed70d0ec1bf18a95f3b6a632401
Francis Antonio <francis.antonio0927@gmail.com>
2024-05-29 22:02:11 +0800

    Adding reducer and ref

---
+++ src/components/useReducer/reducer.tsx
@@ -0,0 +1,5 @@
+export default function UseReducer () {
+    return (
+        <h1 className="font-bold text-2xl mb-10">UseReducer</h1>
+    )
+}
---
+++ src/components/useRef/ref.tsx
@@ -0,0 +1,5 @@
+export default function UseRef () {
+    return (
+        <h1 className="font-bold text-2xl mb-10">UseRef</h1>
+    )
+}
