7571b86090a1605c891d571f8caf39acf9f15a9c
Francis Antonio <francis.antonio0927@gmail.com>
2024-05-31 20:41:57 +0800

    update reducer

--- src/components/useReducer/reducer.tsx
+++ src/components/useReducer/reducer.tsx
@@ -3,47 +3,55 @@ import { useReducer, useRef } from "react"
 
 type State = {
     age: number,
-    name: string
+    name: string,
+    birthdate: string,
 }
 function reducer(state : State, action : {type: string, value: string}){
     switch(action.type){
         case 'increment': {
             return {
-                name: state.name,
+                ...state,
                 age: state.age + 1
             }
         }
 
         case 'decrement': {
             return {
-                name: state.name,
+               ...state,
                 age: state.age - 1
             }
         }
         case 'multiply': {
             return {
-                  name: state.name,
+                 ...state,
                 age: state.age * 2  
             }
         }
         case 'divide': {
             return {
-                  name: state.name,
+                 ...state,
                 age: state.age / state.age
             }
         }
 
         case 'ChangeName': {
             return {
-                age: state.age,
+               ...state,
                 name: action.value
+            }
+        }
+
+        case 'ChangeDate': {
+            return {
+                ...state,
+               birthdate: "09/28/2002",
             }
         }
     }
 }
 
 export default function UseReducer () {
-    const initial = {age: 42, name: ""}
+    const initial = {age: 42, name: "", birthdate: "09/27/2002"}
     const [state, dispatch] = useReducer(reducer, initial)
 
     const Name = useRef<HTMLInputElement>(null)
@@ -59,12 +67,14 @@ export default function UseReducer () {
             <input type="text" className="border border-slate-300" placeholder="Name" ref={Name}/>
             <p>Number: {state.age}</p>
             <p>Name: {state.name}</p>
+            <p>Birthdate: {state.birthdate}</p>
             <div className="space-x-5">
                 <button className="bg-sky-300 px-3" onClick={()=> dispatch({type: 'increment'})}>increment</button>
                 <button className="bg-red-300 px-3" onClick={()=> dispatch({type: 'decrement'})}>decrement</button>
                 <button className="bg-green-300 px-3" onClick={()=> dispatch({type: 'multiply'})}>multiply</button>
                 <button className="bg-orange-300 px-3" onClick={()=> dispatch({type: 'divide'})}>divide</button>
                 <button className="bg-yellow-300 px-3" onClick={handleAdd}>changeName</button>
+                <button className="bg-yellow-300 px-3" onClick={()=> dispatch({type: 'ChangeDate'})}>ChangeDate</button>
             </div>
           
         </>
